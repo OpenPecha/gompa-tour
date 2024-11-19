8d803d291628aa7152697670cbb59ff6342328c2
kunchok tashi <khakontas33@gmail.com>
2024-11-19 14:02:03 -0500

    added qr scanner

--- lib/ui/screen/qr_screen.dart
+++ lib/ui/screen/qr_screen.dart
@@ -1,13 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
-class QrScreen extends ConsumerWidget {
+import '../../helper/database_helper.dart';
+import '../widget/scanner_overlay.dart';
+
+class QrScreen extends ConsumerStatefulWidget {
   const QrScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Center(
-      child: Text('Qr Screen'),
+  ConsumerState createState() => _QrScreenState();
+}
+
+class _QrScreenState extends ConsumerState<QrScreen> {
+  late final MobileScannerController controller;
+  bool isDetecting = false;
+  @override
+  void initState() {
+    controller = MobileScannerController(
+      formats: const [BarcodeFormat.qrCode],
     );
+    controller.start();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final scanWindow = Rect.fromCenter(
+      center: MediaQuery.sizeOf(context).center(Offset.zero),
+      width: 200,
+      height: 200,
+    );
+
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        MobileScanner(
+          scanWindow: scanWindow,
+          fit: BoxFit.contain,
+          controller: controller,
+          onDetect: (data) {
+            if (!isDetecting) {
+              isDetecting = true;
+              _handleQRCodeDetected(data.barcodes[0].rawValue);
+            }
+          },
+        ),
+        ValueListenableBuilder(
+          valueListenable: controller,
+          builder: (context, value, child) {
+            if (!value.isInitialized ||
+                !value.isRunning ||
+                value.error != null) {
+              return const SizedBox();
+            }
+
+            return CustomPaint(
+              painter: ScannerOverlay(scanWindow: scanWindow),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  void _handleQRCodeDetected(String? qrCode) {
+    if (qrCode != null) {
+      // Handle the scanned QR code
+      logger.info('Scanned QR Code: $qrCode');
+
+      // Reset the detection flag after 2 seconds
+      Future.delayed(const Duration(seconds: 1), () {
+        isDetecting = false;
+        if (!mounted) return;
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Scanned QR Code: $qrCode'),
+          ),
+        );
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import audioplayers_darwin
 import flutter_tts
+import mobile_scanner
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
@@ -14,6 +15,7 @@ import sqflite_darwin
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
